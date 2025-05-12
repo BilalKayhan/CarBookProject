4226e520afe854356be9bbcb33e410984f68fc21
BilalKayhan <fbilalkayhan@gmail.com>
2025-05-12 17:19:41 +0300

    Feature sınıfı MediatR kütüphanesi ile yazıldı. Feature api test edildi.

---
+++ Core/CarBook.Application/Features/Mediator/Queries/FeatureQueries/GetFeatureQuery.cs
@@ -0,0 +1,7 @@
+﻿using CarBook.Application.Features.Mediator.Results.FeatureResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+public class GetFeatureQuery : IRequest<List<GetFeatureQueryResult>>
+{
+}
---
+++ Core/CarBook.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CarBook.Application.Services;
+public static class ServiceRegistiration
+{
+    public static void AddApplicationService(this IServiceCollection services, Microsoft.Extensions.Configuration.IConfiguration configuration)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly));
+    }
+}
