1b0a327cbb25a4255c1bd3882629175f1930e46c
BilalKayhan <fbilalkayhan@gmail.com>
2025-05-13 17:00:45 +0300

    Service sınıfı MediatR kütüphanesi ile yazıldı. Service api test edildi.

---
+++ Core/CarBook.Application/Features/Mediator/Queries/ServiceQueries/GetServiceQuery.cs
@@ -0,0 +1,7 @@
+﻿using CarBook.Application.Features.Mediator.Results.ServiceResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Queries.ServiceQueries;
+public class GetServiceQuery : IRequest<List<GetServiceQueryResult>>
+{
+}
