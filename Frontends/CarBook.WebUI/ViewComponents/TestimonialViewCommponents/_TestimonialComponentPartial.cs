931a1ab0f86d942e3ee51feb2817c898f8528618
BilalKayhan <fbilalkayhan@gmail.com>
2025-05-15 19:35:56 +0300

    Testimonial sınıfı MediatR kütüphanesi ile yazıldı. Testimonial api test edildi.

--- Frontends/CarBook.WebUI/ViewComponents/TestimonialViewCommponents/_TestimonialComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewCommponents/_TestimonialComponentPartial.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.TestimonialViewCommponents;
 
@@ -19,8 +18,8 @@ public class _TestimonialComponentPartial : ViewComponent
         if (responseMessage.IsSuccessStatusCode)
         {
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<List<>>(jsonData);
-            return View(values);
+            //var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+            //return View(values);
         }
         return View();
     }
